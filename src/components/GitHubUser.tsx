361fb593f1c2ba6a7052e250e72a78bec2749e2a
Daniel Castillo <dlcastillo3015@gmail.com>
2023-06-15 05:48:23 -0400

    feat: validate input

--- src/components/GitHubUser.tsx
+++ src/components/GitHubUser.tsx
@@ -1,14 +1,30 @@
 const GitHubUser = () => {
+  async function search() {
+    const $githubUserInput = document.querySelector(
+      "#github-user"
+    ) as HTMLInputElement;
+    const $span = document.querySelector("span");
+
+    if ($githubUserInput.value !== "") {
+      $span?.classList.remove("show");
+      $span?.classList.add("hide");
+    } else {
+      $span?.classList.remove("hide");
+      $span?.classList.add("show");
+    }
+  }
+
   return (
-    <>
+    <div className="form-container">
       <input
         type="text"
         name="github-user"
         id="github-user"
         placeholder="Introduce el nombre de usuario de GitHub"
       />
-      <button>Buscar</button>
-    </>
+      <span className="hide">Introduce un nombre de usuario</span>
+      <button onClick={search}>Buscar</button>
+    </div>
   );
 };
 
