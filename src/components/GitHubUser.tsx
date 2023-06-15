64c17b3ade9c7d2d66198213d118260abd1bdd44
Daniel Castillo <dlcastillo3015@gmail.com>
2023-06-15 08:31:50 -0400

    feat: call api when the user introduce the username

--- src/components/GitHubUser.tsx
+++ src/components/GitHubUser.tsx
@@ -1,4 +1,36 @@
+import { useEffect, useState } from "react";
+import IUserData from "../helpers/interfaces";
+
 const GitHubUser = () => {
+  const [userName, setUserName] = useState("");
+  const [userData, setUserData] = useState<IUserData>();
+
+  useEffect(() => {
+    if (userName !== "") {
+      fetch(`https://api.github.com/users/${userName}`, {
+        method: "GET",
+        redirect: "follow",
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            const $span = document.querySelector("span") as HTMLSpanElement;
+            $span.innerText = "Ese nombre de usuario no existe";
+            $span?.classList.remove("hide");
+            $span?.classList.add("show");
+          }
+        })
+        .then((result) => setUserData(result))
+        .catch(() => {
+          const $span = document.querySelector("span") as HTMLSpanElement;
+          $span.innerText = "Ha ocurrido un error";
+          $span?.classList.remove("hide");
+          $span?.classList.add("show");
+        });
+    }
+  }, [userName]);
+
   async function search() {
     const $githubUserInput = document.querySelector(
       "#github-user"
@@ -8,6 +40,7 @@ const GitHubUser = () => {
     if ($githubUserInput.value !== "") {
       $span?.classList.remove("show");
       $span?.classList.add("hide");
+      setUserName($githubUserInput.value);
     } else {
       $span?.classList.remove("hide");
       $span?.classList.add("show");
